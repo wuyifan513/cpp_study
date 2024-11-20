a63a2779a49b6b1e86eb7264988472c866acec42
wuyifan513 <2229024578@qq.com>
2024-11-20 10:35:00 +0800

    commit My_Pool.hpp

---
+++ inc/My_Pool.hpp
@@ -0,0 +1,52 @@
+#include<atomic>
+#include<queue>
+#include<vector>
+#include<condition_variable>
+#include<iostream>
+#include<mutex>
+#include<thread>
+using namespace std;
+template <class T>
+class My_Pool
+{
+private:
+    /* data */
+    atomic_bool is_running;
+    int num_thread;
+    condition_variable cv;
+    mutex mux;
+    queue<Task> q;
+    vector<thread> workers;
+    void work(){
+        cout<<this_thread::get_id()<< endl;
+        while(is_running)
+        {
+            Task t;
+            {
+
+            
+                unique_lock<mutex> lk(mux);
+                if(!q.empty()){
+                    t = q.front();
+                    q.pop();
+                }
+                else if(is_running&&q.empty()) cv.wait(lk);
+            }
+            if(t) t();
+        }
+    }
+public:
+    using Task = function<void()>;
+    explicit My_Pool(int num):num_thread(num), is_running(false){};
+    void start(){
+        {
+            unique_lock<mutex> lk(mux);
+            is_running = true;
+        }
+        for(int i = 0; i < num_thread; i ++){
+            workers.push_back(thread(&My_Pool::work, &My_Pool));
+        }
+    }
+    ~My_Pool(){};
+};
+
